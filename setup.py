85eed7d711f2af42f07e43ed76bc5bf4fa698500
aliasgar1978 <30945776+aliasgar1978@users.noreply.github.com>
2021-03-11 08:49:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nettoolkit",
-    version="0.0.3",
+    version="0.0.4",
     author="ALIASGAR - ALI",
     author_email="aholo2000@gmail.com",
     description="Tool Set for Networking Geeks",

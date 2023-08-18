0a03a29495579404eb3f6287b90b402423223464
aliasgar1978 <aholo2000@gmail.com>
2023-08-18 15:06:30 +0530

    ver0023 initial push

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, ALIASGAR [ALI]'
 author = 'ALIASGAR [ALI]'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.22'
+release = '0.0.23'
 
 
 # -- General configuration ---------------------------------------------------
--- nettoolkit/__init__.py
+++ nettoolkit/__init__.py
@@ -54,7 +54,7 @@ __all__ = [
 
 	]
 
-__version__ = "0.0.22"
+__version__ = "0.0.23"
 
 from .juniper import Juniper
 from .jset import JSet

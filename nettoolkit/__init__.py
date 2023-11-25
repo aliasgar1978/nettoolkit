e2ca2a268557e69dde7abb3396f427760b1a403b
aliasgar1978 <aholo2000@gmail.com>
2023-11-25 16:58:52 +0530

    .

--- nettoolkit/__init__.py
+++ nettoolkit/__init__.py
@@ -1,7 +1,7 @@
 __doc__ = '''Networking ToolSet
 '''
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 
 def version():
--- nettoolkit/nettoolkit_db/__init__.py
+++ nettoolkit/nettoolkit_db/__init__.py
@@ -10,7 +10,7 @@ __all__ = [
 ]
 
 
-__version__ = "0.0.2"
+__version__ = "1.2.0"
 
 
 from .convertdict import ConvDict

94cf322c5cd1821a8364df3cc7f0da237934dc13
aliasgar1978 <aholo2000@gmail.com>
2024-03-25 10:23:31 +0530

    .

--- nettoolkit/__init__.py
+++ nettoolkit/__init__.py
@@ -9,34 +9,34 @@ def version():
 ## --- IMPORTS --- ##
 
 # ---------------- nettoolkit-common ---------------- #
-from .nettoolkit_common.gpl import (Default, Container, Numeric, 
+from nettoolkit.nettoolkit_common.gpl import (Default, Container, Numeric, 
 	DifferenceDict, DictMethods, DIC,
 	STR, IO, LST, LOG, DB, IP, XL_READ, XL_WRITE, 
 	Multi_Execution, nslookup, standardize_if,
 	get_username, get_password, 
 	get_juniper_int_type, get_cisco_int_type, get_device_manu
 )
-from .nettoolkit_common.common import (
+from nettoolkit.nettoolkit_common.common import (
 	remove_domain, read_file, get_op, get_ops, blank_line, get_device_manufacturar, verifid_output, 
 	get_string_part, get_string_trailing, standardize_mac, mac_2digit_separated, mac_4digit_separated,
 	flatten, dataframe_generate
 )
 
 # ---------------- nettoolkit-db ---------------- #
-from .nettoolkit_db.convertdict import ConvDict
-from .nettoolkit_db.database import write_to_xl, append_to_xl, read_xl, get_merged_DataFrame_of_file
+from nettoolkit.nettoolkit_db.convertdict import ConvDict
+from nettoolkit.nettoolkit_db.database import write_to_xl, append_to_xl, read_xl, get_merged_DataFrame_of_file
 
 # ---------------- pyNetCrypt ---------------- #
-from .pyNetCrypt.cpw_cracker import decrypt_type7, encrypt_type7, decrypt_file_passwords, mask_file_passwords
-from .pyNetCrypt.jpw_cracker import juniper_decrypt, juniper_encrypt, decrypt_doller9_file_passwords, mask_doller9_file_passwords
-from .pyNetCrypt.generate import get_md5
+from nettoolkit.pyNetCrypt.cpw_cracker import decrypt_type7, encrypt_type7, decrypt_file_passwords, mask_file_passwords
+from nettoolkit.pyNetCrypt.jpw_cracker import juniper_decrypt, juniper_encrypt, decrypt_doller9_file_passwords, mask_doller9_file_passwords
+from nettoolkit.pyNetCrypt.generate import get_md5
 
 # ---------------- pyJuniper ---------------- #
-from .pyJuniper.juniper import Juniper, convert_to_set_from_captures
-from .pyJuniper.jset import JSet
+from nettoolkit.pyJuniper.juniper import Juniper, convert_to_set_from_captures
+from nettoolkit.pyJuniper.jset import JSet
 
 # ---------------- nettoolkit ---------------- #
-from .nettoolkit.gui import Nettoolkit
+from nettoolkit.nettoolkit.gui import Nettoolkit
 
 
 ## --- DECLARATIONS --- ##

41a33c18aa96a331f70df4d910390f5e4a67eba8
aliasgar1978 <aholo2000@gmail.com>
2024-09-15 13:31:56 +0530

    .

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ copyright = '2023, ALIASGAR [ALI]'
 author = 'ALIASGAR [ALI]'
 
 # The full version, including alpha/beta/rc tags
-release = '1.7.4'
+release = '1.7.5'
 
 # -- General configuration ---------------------------------------------------
 

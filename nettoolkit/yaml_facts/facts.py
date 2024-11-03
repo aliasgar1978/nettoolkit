f37a65ac1814e609fbc97216de905abf6cbd8c05
aliasgar1978 <aholo2000@gmail.com>
2024-11-03 13:01:53 +0530

    .

--- nettoolkit/yaml_facts/facts.py
+++ nettoolkit/yaml_facts/facts.py
@@ -46,17 +46,4 @@ class YamlFacts():
 # ==============================================================================================
 if __name__ == '__main__':
 	pass
-	# file = "C:/Users/al202t/Documents/A T M/Captures/530-vua-1f1.log"
-	file = "C:/Users/al202t/Documents/A T M/Captures/530-end.log"
-	# file = 'C:/Users/al202t/Documents/A T M/Captures/z3o-ecd-b.log'
-
-	FR = YamlFacts(file)	
-	# pprint(FR.captures.outputs)
-	# pprint(FR.captures.device_manufacturar)
-	# print(FR.captures.outputs.keys())
-	# pprint(FR.captures.cmd_output("show running-config"))
-	# print(FR.captures.has("show version"))
-
-	# print(FR.CP.output_yaml)
-
 # ==============================================================================================

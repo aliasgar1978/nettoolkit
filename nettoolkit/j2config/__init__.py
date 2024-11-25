1aa02a186dcd0aa71cdec99c8f39d61a633655f4
aliasgar1978 <aholo2000@gmail.com>
2024-11-25 19:07:20 +0530

    .

--- nettoolkit/j2config/__init__.py
+++ nettoolkit/j2config/__init__.py
@@ -4,12 +4,13 @@ __doc__ = '''Package which helps simplifying text output generation from templat
 from .j2 import PrepareConfig
 from .read_conditions import get_variables, get_conditions, JinjaVarCheck
 from .data_collect import ABSRegion
-
+from .exec_fns import exec_config_generation, get_host
 
 __all__ = [
 	'PrepareConfig',
 	'get_conditions', 'get_variables', 'JinjaVarCheck',
-	'ABSRegion'
+	'ABSRegion',
+	'exec_config_generation', 'get_host',
 	
 ]
 
--- nettoolkit/j2config/forms/execs.py
+++ nettoolkit/j2config/forms/execs.py
@@ -1,10 +1,10 @@
 
 from nettoolkit.nettoolkit.forms.formitems import *
-from nettoolkit.nettoolkit_common import read_yaml_mode_us, create_folders, open_text_file, open_folder, open_excel_file, print_banner
+from nettoolkit.nettoolkit_common import create_folders, open_text_file, open_folder, open_excel_file
 from pathlib import *
 import sys
 
-from nettoolkit.j2config import PrepareConfig
+from nettoolkit.j2config import exec_config_generation, get_host
 from nettoolkit.j2config.general import get_model_template_version
 
 
@@ -30,57 +30,26 @@ def exec_j2_files_output_open(i):
 	for file in i['j2_output_files'].split(";"):
 		open_text_file(file)
 
-def get_host(file):
-	return Path(file).stem.split("-clean")[0]
-
 def add_path(file):
 	sys.path.insert(len(sys.path), str(Path(file).resolve().parents[0]))
 
 
-def get_custom_classes(custom):
-	return {k: v for k, v in custom['j2_class_filters'].items() }
-
-def get_custom_funcs(custom):
-	return { v for k, v in custom['j2_functions_filters'].items() }
-
-
 @activity_finish_popup
 def j2config_start(obj, i):
-	if i['j2_file_custom_yml']:
-		add_path(i['j2_file_custom_yml'])
-		custom =  read_yaml_mode_us(i['j2_file_custom_yml']) 
-	#
+	custom =  i['j2_file_custom_yml']
+	if custom:
+		add_path(custom)
 	regional_file = i['j2_file_regional'] if i['j2_file_regional'] else None
-	regional_class = custom['j2_regional']['regional_class'] if i['j2_file_custom_yml'] else None
 	#
-	print_banner("Config Gen", 'green')
+	exec_config_generation(
+		data_files=i['j2_file_data'].split(";"),
+		template_file=i['j2_file_template'],
+		output_folder=i['j2_output_folder'],
+		regional_file=regional_file, 
+		custom=i['j2_file_custom_yml'],
+	)
 	#
-	for data_file in i['j2_file_data'].split(";"):
-		if not data_file.endswith(".xlsx"): continue
-		device = get_host(data_file)
-		print(f"Generating Configuration for {device}", end="\t")
-		try:
-			PrCfg = PrepareConfig(
-				data_file=data_file,
-				jtemplate_file=i['j2_file_template'],
-				output_folder=i['j2_output_folder'],
-				regional_file=regional_file,
-				regional_class=regional_class,
-			)
-			custom_classes = get_custom_classes(custom)
-			custom_funcs = get_custom_funcs(custom)
-			#
-			PrCfg.custom_class_add_to_filter(**custom_classes)
-			PrCfg.custom_module_methods_add_to_filter(*custom_funcs)
-			#
-			PrCfg.start()
-			print(f"Done..")
-		except:
-			print(f"Failed..")
-
 	update_output_files(obj, i)
-
-	print("Configuration Generation All Task(s) Complete..")
 
 
 def update_output_files(obj, i):
